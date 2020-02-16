a57496fe80ad3a7903205558896ca31c3f5a3017
Michael McDonald <mchlmcdnld7@gmail.com>
2020-02-16 22:33:40 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -128,5 +128,5 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-programmer_hash[:dennis_ritchie][:languages].merge("Assembly")
+programmer_hash[:dennis_ritchie][:languages].push("Assembly")
 end
